45d073254234f24bb603d4d711b7601cbf234156
Diogo <diogo3679@gmail.com>
2022-01-15 18:18:26 +0000

    Co-authored-by: AndreLima001 <AndreLima001@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,20 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Weather App '),
     );
   }
 }
@@ -48,68 +40,106 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<WeatherInfo>? _weatherInfo;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // Location
+  Location location = Location();
+  bool _serviceEnabled = false;
+  PermissionStatus _permissionStatus = PermissionStatus.denied;
+  LocationData? _locationData;
+  String? _weatherInfoUrl;
+  /*static const String _weatherInfoUrl =
+      'https://api.weatherapi.com/v1/forecast.json?key=a62d114fc8f54d9dbc8153818210612&q=' +
+          _locationData!.latitude +
+          "," +
+          _locationData!.longitude +
+          '&days=3&aqi=no&alerts=yes';*/
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchLocation();
+    _getUrlWeatherLocation();
+  }
+
+  //Future<void> _fetchWeatherInfo() async {
+  // try {} catch (ex) {
+  //  debugPrint('Error in geting weather info:$ex');
+  // }
+  // }
+
+  Future<void> _fetchLocation() async {
+    // Verificar estado do serviço
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+    }
+
+    // Pede permissões em runtime
+    _permissionStatus = await location.hasPermission();
+    if (_permissionStatus == PermissionStatus.denied) {
+      _permissionStatus = await location.requestPermission();
+    }
+
+    await _getCoordinates();
+  }
+
+  Future<void> _getCoordinates() async {
+    _locationData = await location.getLocation();
+    setState(() {});
+  }
+
+  Future<void> _getUrlWeatherLocation() async {
+    if (!_serviceEnabled)
+      debugPrint('Location service not enabled');
+    else if (_permissionStatus == PermissionStatus.denied) {
+      debugPrint('Location service not enabled');
+    } else if (_locationData != null) {
+      _weatherInfoUrl =
+          'https://api.weatherapi.com/v1/forecast.json?key=a62d114fc8f54d9dbc8153818210612&q=' +
+              _locationData!.latitude.toString() +
+              "," +
+              _locationData!.longitude.toString() +
+              '&days=3&aqi=no&alerts=yes';
+    }
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (!_serviceEnabled)
+            const Text('Location service not enabled')
+          else if (_permissionStatus == PermissionStatus.denied)
+            const Text("Location service not allowed")
+          else if (_locationData == null)
+            const CircularProgressIndicator()
+          else
+            StreamBuilder<LocationData>(
+                stream: location.onLocationChanged,
+                builder: (context, snapshot) {
+                  return Text(
+                      'https://api.weatherapi.com/v1/forecast.json?key=a62d114fc8f54d9dbc8153818210612&q=' +
+                          _locationData!.latitude.toString() +
+                          "," +
+                          _locationData!.longitude.toString() +
+                          '&days=3&aqi=no&alerts=yes');
+                }),
+        ],
+      )),
     );
   }
+}
+
+class WeatherInfo {
+  //WeatherInfo.fromJson(Map<String, dynamic> json):
+
+  // final String location;
 }
